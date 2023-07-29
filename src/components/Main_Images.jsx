121b21372f1af4843390a47c4a9b93548c5c6e18
Darshan <darshanpatel2923@gmail.com>
2023-07-29 18:02:45 +0530

    Added models for face detection and recognition

---
+++ src/components/Main_Images.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Main_Images() {
+    return (
+        <section id="main">
+            <div>Main_Images</div>
+        </section>
+
+    )
+}
+
+export default Main_Images
